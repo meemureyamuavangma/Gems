5c54ef80f8a851146d1213d3ee899a030a74ea2a
meemureyamuavangma <meemureyamu.avangma@gmail.com>
2026-08-04 10:58:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 */
 
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://supabase.com/dashboard/project/nbawdexultobgdfylwzg/settings/api-keys",
+  SUPABASE_URL: "https://nbawdexultobgdfylwzg.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_gx5hsY4-gtCjcR17AtSaTQ_Lnp3-wj7",
 
   SITE_NAME: "Aurelia",
